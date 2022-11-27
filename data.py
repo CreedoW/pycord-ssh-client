0a133343960b59373e3ffd8ca9c36c7cac10699b
CreedoW <53373412+CreedoW@users.noreply.github.com>
2022-11-27 18:58:39 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
-username = ''
-password = ''
-port = ''
-hostname = ''
-token = ''
+username = '' #user
+password = '' #password
+port = '' #default 22, so if u dont know the port u probably using port 22 
+hostname = '' #server ip
+token = '' #bot token
